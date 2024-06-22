c11fc9eefe79b70d1e9cf6bd5626e1843e603213
Wellington Passos <>
2024-06-22 15:42:38 -0300

    Add interface for communication

---
+++ interfaces/interfaces.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file interfaces.hpp
+ * @author user (contact@email.com)
+ * @brief Insert a brief description here
+ * @date 2024-06-22
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef INTERFACES_HPP_
+#define INTERFACES_HPP_
+
+
+#include "inoutstream/inoutstream.hpp"
+
+
+#endif /* INTERFACES_HPP_ */
